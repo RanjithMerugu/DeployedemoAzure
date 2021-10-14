13cde1b8fd0a0c9344795e4b041b4487b7a6598c
RanjithMerugu <RanjithMerugu@example.com>
2021-10-14 08:09:36 +0530

    first commit

---
+++ src/main/java/com/example/demo/DeployedemoAzureApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployedemoAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployedemoAzureApplication.class, args);
+	}
+
+}
